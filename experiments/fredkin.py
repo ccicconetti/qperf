931616741274d351a6e7d0e9e6df14c98981349e
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2023-09-22 12:39:08 +0200

    Update fredkin.py

--- experiments/fredkin.py
+++ experiments/fredkin.py
@@ -1,30 +1,19 @@
+import logging
 import numpy as np
 
 from netqasm.sdk.qubit import Qubit
 from netqasm.sdk.toolbox.state_prep import set_qubit_state
 
-from netsquid.qubits.dmutil import dm_fidelity
-
-from squidasm.util import get_qubit_state, get_reference_state
 from squidasm.run.stack.config import NVQDeviceConfig, StackConfig, StackNetworkConfig
 from squidasm.run.stack.run import run
 from squidasm.sim.stack.common import LogManager
 from squidasm.sim.stack.program import Program, ProgramContext, ProgramMeta
 
-from utils import getenv_or_default, fredkin
+from utils import getenv_or_default, fredkin, random_init_qubits, fidelity
 
 
 def to_degrees(angle_rad: float) -> int:
     return int(round(angle_rad * 180 / np.pi))
-
-
-def fidelity(qubit: Qubit, phi: float, theta: float) -> float:
-    return dm_fidelity(
-        get_qubit_state(qubit, "client"),
-        get_reference_state(phi, theta),
-        squared=False,
-        dm_check=False,
-    )
 
 
 class ClientProgram(Program):
@@ -37,42 +26,53 @@ class ClientProgram(Program):
             max_qubits=3,
         )
 
+    def __init__(self):
+        self._logger = LogManager.get_stack_logger(self.__class__.__name__)
+        self._errors: int = 0
+
     def run(self, context: ProgramContext):
         conn = context.connection
-        logger = LogManager.get_stack_logger(self.__class__.__name__)
 
-        (phi1, theta1) = (
-            np.random.random() * np.pi / 2,
-            np.random.random() * np.pi / 2,
-        )
-        (phi2, theta2) = (phi1 + np.pi / 3, theta1 + np.pi / 3)
-        logger.info(
-            f"qubit1: phi {to_degrees(phi1)} degrees, theta {to_degrees(theta1)} degrees"
-        )
-        logger.info(
-            f"qubit2: phi {to_degrees(phi2)} degrees, theta {to_degrees(theta2)} degrees"
-        )
+        flip = np.random.random() > 0.5
 
         ctrl = Qubit(conn)
-        ctrl.X()
+        if flip:
+            ctrl.X()
         qubit1 = Qubit(conn)
-        set_qubit_state(qubit1, phi1, theta1)
         qubit2 = Qubit(conn)
-        set_qubit_state(qubit2, phi2, theta2)
+        random_values = random_init_qubits([qubit1, qubit2])
+        assert len(random_values)
+        phi1 = random_values[0][0]
+        phi2 = random_values[1][0]
+        theta1 = random_values[1][1]
+        theta2 = random_values[1][1]
+
+        self._logger.info(
+            f"qubit1: phi {to_degrees(phi1)} degrees, theta {to_degrees(theta1)} degrees"
+        )
+        self._logger.info(
+            f"qubit2: phi {to_degrees(phi2)} degrees, theta {to_degrees(theta2)} degrees"
+        )
 
         fredkin(ctrl, qubit1, qubit2)
         yield from conn.flush()
 
         (fid_1_1, fid_1_2, fid_2_1, fid_2_2) = (
-            fidelity(qubit1, phi1, theta1),
-            fidelity(qubit1, phi2, theta2),
-            fidelity(qubit2, phi1, theta1),
-            fidelity(qubit2, phi2, theta2),
+            fidelity("client", qubit1, phi1, theta1),
+            fidelity("client", qubit1, phi2, theta2),
+            fidelity("client", qubit2, phi1, theta1),
+            fidelity("client", qubit2, phi2, theta2),
         )
 
-        logger.info(
-            f"fidelities {fid_1_1:.2f} {fid_1_2:.2f} {fid_2_1:.2f} {fid_2_2:.2f}"
+        self._logger.info(
+            f"flip {flip} fidelities {fid_1_1:.2f} {fid_1_2:.2f} {fid_2_1:.2f} {fid_2_2:.2f}"
         )
+
+        fid_prod_higher = fid_1_1 * fid_2_2 if not flip else fid_1_2 * fid_2_1
+        fid_prod_lower = fid_1_2 * fid_2_1 if not flip else fid_1_1 * fid_2_2
+
+        if fid_prod_higher <= fid_prod_lower:
+            self._errors += 1
 
         return {}
 
@@ -89,4 +89,10 @@ if __name__ == "__main__":
     )
     cfg = StackNetworkConfig(stacks=[client], links=[])
 
-    run(cfg, {"client": ClientProgram()}, num_times=10)
+    num_times = int(getenv_or_default("NUM_ITERATIONS", "100"))
+    client_program = ClientProgram()
+    client_program._logger.setLevel(logging.INFO)
+
+    run(cfg, {"client": client_program}, num_times=num_times)
+
+    print(f"{client_program._errors / num_times}")
--- experiments/utils.py
+++ experiments/utils.py
@@ -1,7 +1,13 @@
 from os import getenv
+import numpy as np
 
 from netqasm.sdk.qubit import Qubit
 from netqasm.sdk.toolbox.gates import toffoli_gate
+from netqasm.sdk.toolbox.state_prep import set_qubit_state
+
+from netsquid.qubits.dmutil import dm_fidelity
+
+from squidasm.util import get_qubit_state, get_reference_state
 
 
 def fredkin(control: Qubit, input1: Qubit, input2: Qubit) -> None:
@@ -22,3 +28,26 @@ def getenv_or_default(env_var: str, default_value: str) -> str:
         return default_value
     assert isinstance(value, str)
     return value
+
+
+def random_init_qubits(qubits: list) -> list:
+    ret = []
+    for qubit in qubits:
+        (phi, theta) = (
+            np.random.random() * np.pi,
+            np.random.random() * np.pi,
+        )
+        ret.append((phi, theta))
+
+        set_qubit_state(qubit, phi, theta)
+
+    return ret
+
+
+def fidelity(program_name: str, qubit: Qubit, phi: float, theta: float) -> float:
+    return dm_fidelity(
+        get_qubit_state(qubit, program_name),
+        get_reference_state(phi, theta),
+        squared=False,
+        dm_check=False,
+    )
